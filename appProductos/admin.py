2e782d5238ea2cc784b1af03a19914b59cca104d
marcoleonmora <mlmoram@misena.edu.co>
2022-10-27 15:39:08 -0500

    configurar Admin

--- appProductos/admin.py
+++ appProductos/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+#---------------------------------------------
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['id','descripCategoria']
+
+admin.site.register(Categoria, CategoriaAdmin)
+
+#---------------------------------------------
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre','imgGrande', 'imgPeque']
+
+admin.site.register(Producto, ProductoAdmin)
+
+#---------------------------------------------
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ['usuario','producto', 'cantidad', 'estado']
+
+admin.site.register(Carro, CarroAdmin)
+
