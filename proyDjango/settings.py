063d33311d98c72148d898de38b2338ae1fab2cd
marcoleonmora <mlmoram@misena.edu.co>
2022-10-27 14:33:11 -0500

    Factorizacion

--- proyDjango/settings.py
+++ proyDjango/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'proyDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = ['proyDjango/static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
